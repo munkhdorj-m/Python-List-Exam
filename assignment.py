ee8ef7fc757fdcccce099295cdf0918975182583
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2026-05-13 10:30:16 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -4,59 +4,25 @@ def sum_elements(lst):
 
 
 # Exercise 2
-
 def find_smallest(lst):
-    smallest = lst[0]
-    for num in lst:
-        if num < smallest:
-            smallest = num
-    return smallest
+    pass
 
 
 # Exercise 3
 def count_occurrences(lst, target):
-    count = 0
-    for num in lst:
-        if num == target:
-            count += 1
-    return count
+    pass
 
 
 # Exercise 4
 def move_zeroes(lst):
-    result = []
-
-    for num in lst:
-        if num != 0:
-            result.append(num)
-
-    zero_count = len(lst) - len(result)
-
-    for _ in range(zero_count):
-        result.append(0)
-
-    return result
+    pass
 
 
 # Exercise 5
 def find_indices(lst, target):
-    indices = []
-
-    for i in range(len(lst)):
-        if lst[i] == target:
-            indices.append(i)
-
-    return indices
+    pass
 
 
 # Exercise 6
 def mirror_list(lst):
-    result = []
-
-    for num in lst:
-        result.append(num)
-
-    for i in range(len(lst) - 1, -1, -1):
-        result.append(lst[i])
-
-    return result
+    pass
